72ca1aece0c19ee155474ffef2dc382d91e58397
“lukenilsson” <luke.nilsson@gmail.com>
2023-05-11 14:25:06 -0400

    home page css

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,17 @@
+import Scout_1 from "./images/MJ_Scout_1.png";
+
 export function Home() {
   return (
-    <div>
-      <h1>Welcome to Scout</h1>
-      <p>Chewy is selling # of products</p>
+    <div className="page-container">
+      <div className="centered-container">
+        <div className="centered-content">
+          <h1>Welcome to Scout</h1>
+          <div className="home-image">
+            <img src={Scout_1} alt="My Image" />
+          </div>
+          <p>Chewy is selling # of products</p>
+        </div>
+      </div>
     </div>
   );
 }
